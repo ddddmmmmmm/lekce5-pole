ab2435e02b7ec37f226abf85f2830b057cfe1583
Dana <markova.dana@email.cz>
2022-03-18 19:51:33 +0100

    pole_final

--- main.js
+++ main.js
@@ -6,23 +6,31 @@
 // Úkol 1 - Založ nové pole se seznamem jmen (alespoň 5)
 // Vypiš jeho obsah do stránky - JavaScript spojí pole automaticky do řetězce
 
+let pole = [
+    "Alena",
+    "Jana",
+    "Marie",
+    "Filomena",
+    "Hortenzie"
+];
 
+function vypisText(txt) {
+    document.querySelector('#vystup').innerText += txt + '\n';
+}
 
+vypisText(pole)
 
 
 // Úkol 2 - Vypiš třetí jméno v pořadí
 // Nezapomeň, že pole se čísluje od 0
 
-
-
-
-
+vypisText(pole[2])
 
 // Úkol 3 - Kolik jmen je v poli?
 // Pole má vlastnost pole.length
 
-
-
+console.log(pole.length)
+vypisText(pole.length)
 
 
 // Úkol 4 - Vypiš znovu seznam jmen, ALE...
@@ -33,17 +41,19 @@
 // 2. Marie
 // atd.
 
-
-
-
+pole.forEach(function (ele, idx, arr) {
+    console.log(idx + '. ' + ele)
+    vypisText(idx + '. ' + ele)
+})
 
 // Úkol 5 - přidej na konec pole další jméno
 // pole.push()
 // pole.unshift()
 // Doplněný seznam jmen vypiš (jednoduše jako v příkladu 1)
 
-
-
+pole.push("Jan");
+pole.unshift("Anička");
+vypisText(pole)
 
 
 // Úkol 6 - odeber z pole první a poslední jméno a vypiš
@@ -52,15 +62,14 @@
 // pole.pop()
 
 
+let smazePrvni = pole.shift()
+vypisText(smazePrvni)
 
-
-
-
+let smazePosledni = pole.pop()
+vypisText(smazePosledni)
+vypisText(pole)
 
 
 
 
 // *********************************************
-function vypisText(txt) {
-    document.querySelector('#vystup').innerText += txt + '\n';
-}
